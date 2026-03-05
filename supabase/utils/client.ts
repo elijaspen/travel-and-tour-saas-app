0942aa8d5c1ad09423e5bab8283764f67a2c0640
Alanray Elangos <ealanray@gmail.com>
2026-03-05 12:25:51 +0800

    feat: config supabase utils

--- supabase/utils/client.ts
+++ supabase/utils/client.ts
@@ -1,7 +1,7 @@
 "use client";
 
-import { Database } from "@/types";
 import { createBrowserClient } from "@supabase/ssr";
+import { Database } from "../types";
 
 export function createClient() {
   return createBrowserClient<Database>(
