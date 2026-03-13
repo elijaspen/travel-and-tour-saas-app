36cef6d699192a11d0f20023bdd4d3ee69ac8e82
Alanray Elangos <ealanray@gmail.com>
2026-03-13 08:50:44 +0800

    fix: remove old supabase config file

--- supabase/utils/admin.ts
+++ supabase/utils/admin.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@supabase/types";
-import { getSupabaseAdminEnv } from "./config";
 
 export function createAdminClient() {
  const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
